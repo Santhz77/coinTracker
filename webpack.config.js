ce716c8a2cb3054af01fb571bf910b191b863143
Santhz77 <santhoshnayak0903gmail.com>
2022-03-15 14:57:29 +0100

    updated the webpack config to get the local system working

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,69 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
-
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist')
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+    assetModuleFilename: 'images/[hash][ext][query]'
   },
 
-  devtool: 'eval-source-map',
+  devtool: "eval-source-map",
   devServer: {
-    contentBase: './dist'
+    contentBase: "./dist",
   },
 
   module: {
     rules: [
-      
+      // {
+      //   test:  /\.(scss|css)$/,
+      //   use: [
+      //     // Creates `style` nodes from JS strings
+      //     "style-loader",
+      //     // Translates CSS into CommonJS
+      //     "css-loader",
+
+      //     // Compiles Sass to CSS
+      //     "sass-loader",
+      //   ],
+      // },
+
       {
-        test: /\.scss$/,
-        use: [
-            "style-loader",
-            "css-loader",
-            "sass-loader"
-        ]
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+
+            {
+        test: /\.html$/,
+        use: ["html-loader"],
       },
 
       {
         test: /\.(gif|png|jpe?g)$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'assets/images/'
-            }
-          }
-        ]
-      },
+        type: 'asset/resource'
 
-      {
-        test:/\.html$/,
-        use: [
-          'html-loader'
-        ]
+        // use: [
+        //   {
+        //     loader: "file-loader",
+        //     options: {
+        //       name: "[name].[ext]",
+        //       outputPath: "assets/images/",
+        //     },
+        //   },
+        // ],
       },
-
-    ]
+    ],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
-      inject: 'body',
-      template: './src/index.html',
-      filename: 'index.html',
-    })
+      inject: "body",
+      template: "./src/index.html",
+      filename: "index.html",
+    }),
 
-  ]
-
+    new MiniCssExtractPlugin(),
+  ],
 };
